2151bca73ecee53e074c3cf4d76252c815771b4d
Qingquan Li <liqqisme@gmail.com>
2021-09-29 19:04:00 -0400

    add words_in_sentences to INSTALLED_APPS setting

--- a_project_config/settings.py
+++ a_project_config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'words_in_sentences.apps.WordsInSentencesConfig',
 ]
 
 MIDDLEWARE = [
