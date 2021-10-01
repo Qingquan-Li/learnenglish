6d9264297a8d312ff2b6422e9bfc506351dedfca
Qingquan Li <liqqisme@gmail.com>
2021-10-01 18:17:10 -0400

    add accounts/tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='Jake',
+            email='jake@email.com',
+            password='testpass123'
+        )
+        self.assertEqual(user.username, 'Jake')
+        self.assertEqual(user.email, 'jake@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@email.com',
+            password='testpass123'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+
+"""
+$ python manage.py test accounts.tests  # Use SQLite Database
+Creating test database for alias 'default'...
+System check identified no issues (0 silenced).
+..
+----------------------------------------------------------------------
+Ran 2 tests in 0.085s
+
+OK
+Destroying test database for alias 'default'...
+"""
