6d0eef40ff50c1e976d095a7191b2ca59ba99a29
Qingquan Li <liqqisme@gmail.com>
2021-10-01 12:49:38 -0400

    add comments to accounts/models

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,23 @@
 # 2. Delete the db.sqlite3 from the root directory.
 # 3. Rerun: $ python manage.py makemigrations and $ python manage.py migrate
 
+# $ sqlite3 './db.sqlite3'
+# SQLite version 3.32.3 2020-06-18 14:16:19
+# Enter ".help" for usage hints.
+# sqlite> .table
+# accounts_customuser                   auth_permission
+# accounts_customuser_groups            django_admin_log
+# accounts_customuser_user_permissions  django_content_type
+# auth_group                            django_migrations
+# auth_group_permissions                django_session
+
+# The default original database table:
+# auth_group                  auth_user_user_permissions
+# auth_group_permissions      django_admin_log
+# auth_permission             django_content_type
+# auth_user                   django_migrations
+# auth_user_groups            django_session
+
 
 from django.contrib.auth.models import AbstractUser
 
