30368bfbb8172b8c08fb5f1cabb195858aaa7b84
Qingquan Li <liqqisme@gmail.com>
2021-10-02 00:11:52 -0400

    set DEBUG = True

--- a_project_config/settings.py
+++ a_project_config/settings.py
@@ -31,8 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', 'jakeli.pythonanywhere.com']
