e3e3e60be2d881156753dd1d895ab64226e173be
Qingquan Li <liqqisme@gmail.com>
2021-10-26 22:36:25 -0400

    change ModelTest Class's name

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
-class CustomUserTests(TestCase):
+class CustomUserModelTests(TestCase):
 
     def test_create_user(self):
         User = get_user_model()
