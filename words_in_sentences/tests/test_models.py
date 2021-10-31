b558c30c8a08b10bd3bf9496c73df2684b50c195
Qingquan Li <liqqisme@gmail.com>
2021-10-30 23:13:59 -0400

    Improve words_in_sentences.tests.test_models

--- words_in_sentences/tests/test_models.py
+++ words_in_sentences/tests/test_models.py
@@ -8,7 +8,7 @@ class SentenceModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls) -> None:
-        Sentence.objects.create(
+        cls.sentence = Sentence.objects.create(
             # id,
             # created_at,
             # modified_at,
@@ -27,68 +27,64 @@ class SentenceModelTest(TestCase):
         )
 
     def test_version_content(self) -> None:
-        sentence = Sentence.objects.get(id=1)
-        expected_object_name = f'{sentence.version}'
+        expected_object_name = f'{self.sentence.version}'
         self.assertEqual(expected_object_name, '1')
-    
+
     def test_is_active(self) -> None:
-        sentence = Sentence.objects.get(id=1)
-        self.assertTrue(sentence.is_active)
+        self.assertTrue(self.sentence.is_active)
 
     def test_english_sentence_content(self) -> None:
-        sentence = Sentence.objects.get(id=1)
-        expected_object_name = f'{sentence.english_sentence}'
+        expected_object_name = f'{self.sentence.english_sentence}'
         self.assertEqual(expected_object_name, 'first english sentence')
 
     def test_highlight_word_content(self) -> None:
-        sentence = Sentence.objects.get(id=1)
-        expected_object_name = f'{sentence.highlight_word}'
+        expected_object_name = f'{self.sentence.highlight_word}'
         self.assertEqual(expected_object_name, 'english')
 
     def test_chinese_translation_content(self) -> None:
-        sentence = Sentence.objects.get(id=1)
-        expected_object_name = f'{sentence.chinese_translation}'
+        expected_object_name = f'{self.sentence.chinese_translation}'
         self.assertEqual(expected_object_name, '第一个英文句子')
 
     def test_note_content(self) -> None:
-        sentence = Sentence.objects.get(id=1)
-        expected_object_name = f'{sentence.note}'
+        expected_object_name = f'{self.sentence.note}'
         self.assertEqual(
             expected_object_name,
             'english: /ˈiNG(ɡ)liSH/  [Noun] 英语，英国人  [Verb] 英国化  [Adjective] 英国人的'
         )
 
-    def test_get_absolute_url(self):
-        sentence = Sentence.objects.get(id=1)
+    def test_get_absolute_url(self) -> None:
         # This will also fail if the urlconf is not defined.
         self.assertEqual(
-            sentence.get_absolute_url(), '/api/v1/words-in-sentences/sentences/1/')
+            # self.sentence.get_absolute_url(), '/api/v1/words-in-sentences/sentences/1/'
+            self.sentence.get_absolute_url(),
+            '/api/v1/words-in-sentences/sentences/{id}/'.format(id=self.sentence.pk)
+        )
 
 
 class TagModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls) -> None:
-        Tag.objects.create(
+        cls.tag = Tag.objects.create(
             # id,
             name='first tag',
             is_active=0,  # or is_active='0',
         )
 
     def test_name_content(self) -> None:
-        tag = Tag.objects.get(id=1)
-        expected_object_name = f'{tag.name}'
+        expected_object_name = f'{self.tag.name}'
         self.assertEqual(expected_object_name, 'first tag')
 
     def test_is_active(self) -> None:
-        tag = Tag.objects.get(id=1)
-        self.assertFalse(tag.is_active)
+        self.assertFalse(self.tag.is_active)
 
-    def test_get_absolute_url(self):
-        tag = Tag.objects.get(id=1)
+    def test_get_absolute_url(self) -> None:
         # This will also fail if the urlconf is not defined.
         self.assertEqual(
-            tag.get_absolute_url(), '/api/v1/words-in-sentences/tags/1/')
+            # self.tag.get_absolute_url(), '/api/v1/words-in-sentences/tags/1/'
+            self.tag.get_absolute_url(),
+            '/api/v1/words-in-sentences/tags/{id}/'.format(id=self.tag.pk)
+        )
 
 
 """
@@ -97,7 +93,7 @@ Creating test database for alias 'default'...
 System check identified no issues (0 silenced).
 ..........
 ----------------------------------------------------------------------
-Ran 10 tests in 0.016s
+Ran 10 tests in 0.009s
 
 OK
 Destroying test database for alias 'default'...
