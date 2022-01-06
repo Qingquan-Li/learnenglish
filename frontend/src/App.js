be839f698eae1bf2f97843eeb64afd6be899ff8d
Qingquan Li <liqqisme@gmail.com>
2022-01-06 14:03:28 -0500

    Write a simple get-api page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios'; // new
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // state = {
+  //   todos: []
+  // };
+  constructor(props) {
+    // In JavaScript classes, you need to always call super when defining the constructor of a subclass.
+    // All React component classes that have a constructor should start with a super(props) call.
+    super(props);
+    this.state = {
+      sentences: []
+    };
+  }
+
+  // new
+  componentDidMount() {
+    this.getSentences();
+  }
+
+  // new
+  getSentences() {
+    axios
+      .get('http://localhost:8000/api/v1/words-in-sentences/sentences/')
+      .then(res => {
+        this.setState({ sentences: res.data.results });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.sentences.map(item => (
+          <div key={item.id}>
+            <h4>{item.english_sentence}</h4>
+            <span style={{color: "green"}}>{item.highlight_word}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
